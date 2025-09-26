8cd0a504a65e865891fded8cb4da4b84d47ecfe8
Husam Elayyan <husam.elayyan204@gmail.com>
2025-09-26 08:44:41 +0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+  proxy: {
+    '/api': {
+      target: 'http://aliaaorder.runasp.net',
+      changeOrigin: true,
+      secure: false
+    }
+  }
+}
 })
